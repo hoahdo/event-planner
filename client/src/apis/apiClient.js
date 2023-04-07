51ac19228e539120a08cb4cec4f0c5e3a80b9415
Hoa Do <hoa.do.ma@gmail.com>
2023-04-07 02:37:22 -0400

    fix axios config

--- client/src/apis/apiClient.js
+++ client/src/apis/apiClient.js
@@ -5,7 +5,7 @@ const axiosClient = axios.create({
 		? `${import.meta.env.VITE_API_BASE_URL}`
 		: "http://localhost:3000",
 	headers: {
-		Accept: "application/json",
+		"Accept": "application/json",
 		"Content-Type": "application/json",
 	},
 });
