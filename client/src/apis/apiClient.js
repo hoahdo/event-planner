a4eba5e2c53af7532b8c89c6d295b348f25c65bc
Hoa Do <hoa.do.ma@gmail.com>
2023-04-07 02:23:58 -0400

    update api base url

--- client/src/apis/apiClient.js
+++ client/src/apis/apiClient.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-    // baseURL: `${import.meta.env.VITE_API_BASE_URL}`
-    baseURL: `http://localhost:3000`,
-    headers: {
-        "Accept": "application/json",
-        "Content-Type": "application/json"
-    }
-})
+	baseURL: import.meta.env.VITE_API_BASE_URL
+		? `${import.meta.env.VITE_API_BASE_URL}`
+		: "http://localhost:3000",
+	headers: {
+		Accept: "application/json",
+		"Content-Type": "application/json",
+	},
+});
 
 export default axiosClient;
